81a219a97b3567db3d264d3cd9b22f06ad4dd468
Ivan Soto <ivansotocaro30@gmail.com>
2020-10-21 15:38:07 -0500

    Add Template

--- Views/Dashboard/partials/header.php
+++ Views/Dashboard/partials/header.php
@@ -15,7 +15,7 @@
 
 
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="dashboard">Vali</a>
+    <header class="app-header"><a class="app-header__logo" href="dashboard">Framework Mini</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
